8fde26d5f462b3e4357ae6b29fef7e446c608ba7
Kejdi <kejdimuci.dev@gmail.com>
2026-03-28 00:07:04 +0100

    chore: restructure to app and backend

--- web/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom';  // ← add this line at the top
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders trip planner heading', () => {
-  render(<App />);
-  const heading = screen.getByRole('heading', { name: /Trip Planner/i });
-  expect(heading).toBeInTheDocument();
-});
